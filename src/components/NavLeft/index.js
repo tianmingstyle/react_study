698d1c43b50a7ba5d6779958a2b5de6081f29801
tianmingstyle <2059708026@qq.com>
2019-02-24 21:32:24 +0800

    Add files via upload

--- src/components/NavLeft/index.js
+++ src/components/NavLeft/index.js
@@ -1,29 +1,30 @@
 import React from 'react'
 import { Menu, Icon } from 'antd'
 import './index.less'
-import menuConfig from '../../config/menuConfig'
+import MenuConfig from '../../config/menuConfig'
 const SubMenu = Menu.SubMenu
 const MenuItemGroup = Menu.ItemGroup
 
-export default class NalLeft extends React.Component{
+export default class NavLeft extends React.Component{
 	componentWillMount(){
-		const renderMenuNode = this.renderMenu(menuConfig);
+		const renderMenuNode = this.renderMenu(MenuConfig);
 		this.setState({
-			renderMenuNode:renderMenuNode
+			renderMenuNode
 		})
 	}
 	//菜单渲染
 	renderMenu =(data) => {
-		return data.map((item)=>{
+		return (data.map((item)=>{
 			if(item.childern){
 				return (<SubMenu key={item.key} title={item.title}>
-					{this.renderMenu(item.childern)}
-					</SubMenu>)
+						{this.renderMenu(item.childern)}
+						</SubMenu>
+				)
 									
-			}else{
-				return (<SubMenu key={item.key} title={item.title}>{item.title}</SubMenu>)
 			}
-		})
+			return (<Menu.Item key={item.key} title={item.title}>{item.title}</Menu.Item>)
+			
+		}))
 	}
 
 	render(){
@@ -33,9 +34,14 @@ export default class NalLeft extends React.Component{
 						<img src='/asserts/ant-design.svg' alt='' />
 						<h1>Imooc MS</h1>
 					</div>
-					<Menu theme="dark">
+					<Menu theme="dark" mode="inline">
 						{this.state.renderMenuNode}
-
+						<SubMenu key="sub2" title={<span><Icon type="setting" /><span>Navigation Four</span></span>}>
+				            <Menu.Item key="7">Option 7</Menu.Item>
+				            <Menu.Item key="8">Option 8</Menu.Item>
+				            <Menu.Item key="9">Option 9</Menu.Item>
+				            <Menu.Item key="10">Option 10</Menu.Item>
+				         </SubMenu>
 					</Menu>
 				</div>
 			)
